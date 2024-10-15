8227b7403d1d7408e4849330989338c356948ed6
BedirhanAkkurt <98023722+BedirhanAkkurt@users.noreply.github.com>
2024-10-15 19:30:04 +0300

    Update app.js

--- app.js
+++ app.js
@@ -1,126 +1,111 @@
-        // TonConnectUI başlat
-        const tonConnectUI = new TON_CONNECT_UI.TonConnectUI({
-            manifestUrl: 'https://raw.githubusercontent.com/BedirhanAkkurt/randomchat/refs/heads/main/tonconnect-manifest.json',
-            buttonRootId: 'ton-connect' // Buton kök ID'si
-        });
+const canvas = document.getElementById('canvas');
+const ctx = canvas.getContext('2d');
+const img = new Image();
+img.src = "flappy-bird-set.png";
+
+// General settings
+let gamePlaying = false;
+const gravity = 0.5;
+const speed = 6.2;
+const size = [51, 36];
+const jump = -11.5;
+const cTenth = (canvas.width / 10);
+
+let index = 0,
+    bestScore = 0, 
+    flight, 
+    flyHeight, 
+    currentScore, 
+    pipes;
+
+// Pipe settings
+const pipeWidth = 78;
+const pipeGap = 270;
+const pipeLoc = () => (Math.random() * ((canvas.height - (pipeGap + pipeWidth)) - pipeWidth)) + pipeWidth;
+
+const setup = () => {
+    currentScore = 0;
+    flight = jump;
+
+    // Set initial flyHeight (middle of screen - size of the bird)
+    flyHeight = (canvas.height / 2) - (size[1] / 2);
+
+    // Setup first 3 pipes
+    pipes = Array(3).fill().map((a, i) => [canvas.width + (i * (pipeGap + pipeWidth)), pipeLoc()]);
+}
+
+const render = () => {
+    // Make the pipe and bird moving 
+    index++;
+
+    // Background first part 
+    ctx.drawImage(img, 0, 0, canvas.width, canvas.height, -((index * (speed / 2)) % canvas.width) + canvas.width, 0, canvas.width, canvas.height);
+    // Background second part
+    ctx.drawImage(img, 0, 0, canvas.width, canvas.height, -(index * (speed / 2)) % canvas.width, 0, canvas.width, canvas.height);
+  
+    // Pipe display
+    if (gamePlaying){
+        pipes.forEach(pipe => {
+            // Pipe moving
+            pipe[0] -= speed;
+
+            // Top pipe
+            ctx.drawImage(img, 432, 588 - pipe[1], pipeWidth, pipe[1], pipe[0], 0, pipeWidth, pipe[1]);
+            // Bottom pipe
+            ctx.drawImage(img, 432 + pipeWidth, 108, pipeWidth, canvas.height - pipe[1] + pipeGap, pipe[0], pipe[1] + pipeGap, pipeWidth, canvas.height - pipe[1] + pipeGap);
+
+            // Give 1 point & create new pipe
+            if(pipe[0] <= -pipeWidth){
+                currentScore++;
+                // Check if it's the best score
+                bestScore = Math.max(bestScore, currentScore);
+                
+                // Remove & create new pipe
+                pipes = [...pipes.slice(1), [pipes[pipes.length-1][0] + pipeGap + pipeWidth, pipeLoc()]];
+            }
         
-        // Cüzdan bağlantısı olayı dinleyici
-        tonConnectUI.onStatusChange((walletState) => {
-            if (walletState) {
-                console.log("Wallet state received:", walletState);
-
-                // Cüzdan adresini ve publicKey'i alma
-                const user_id = window.Telegram.WebApp.initDataUnsafe.user.id;
-                const walletAddress = walletState.account.address;
-                const publicKey = walletState.account.publicKey;
-
-                // Konsola bastırma (isteğe bağlı, cüzdan bilgilerini burada işleyebilirsin)
-                console.log("Wallet Address:", walletAddress);
-                console.log("Public Key:", publicKey);
-
-                // Bilgileri HTML'de gösterme
-                document.getElementById('walletInfo').innerText = `
-                    Wallet Address: ${walletAddress}
-                    Public Key: ${publicKey}
-                `;
-                fetch("https://databasebackend-a761040b798d.herokuapp.com/api/forward",{
-                        method: "POST",
-                        body: JSON.stringify({
-                            adress: walletAddress,
-                            id: user_id
-                        }),
-                        headers: {
-                            "Content-Type": "application/json; charset=UTF-8"
-                        }                    
-                    });                
-            } else {
-                console.log("No wallet connected.");
+            // If hit the pipe, end
+            if ([pipe[0] <= cTenth + size[0], pipe[0] + pipeWidth >= cTenth, pipe[1] > flyHeight || pipe[1] + pipeGap < flyHeight + size[1]].every(elem => elem)) {
+                gamePlaying = false;
+                setup();
             }
         });
+    }
 
-        // Cüzdan bağlantısı fonksiyonu
-        async function connectToWallet() {
-            try {
-                // Cüzdan bağlantısı sağlanıyor
-                const connectedWallet = await tonConnectUI.connectWallet();
-
-            } catch (error) {
-                console.error("Error connecting to wallet:", error);
-                alert("Error connecting to wallet: " + error.message);
-            }
-        }
-
-        // Cüzdan bağlantısının kontrol edilmesi
-        async function isWalletConnected() {
-            return tonConnectUI.connected;
-        }
+    // Draw bird
+    if (gamePlaying) {
+        ctx.drawImage(img, 432, Math.floor((index % 9) / 3) * size[1], ...size, cTenth, flyHeight, ...size);
+        flight += gravity;
+        flyHeight = Math.min(flyHeight + flight, canvas.height - size[1]);
+    } else {
+        ctx.drawImage(img, 432, Math.floor((index % 9) / 3) * size[1], ...size, ((canvas.width / 2) - size[0] / 2), flyHeight, ...size);
+        flyHeight = (canvas.height / 2) - (size[1] / 2);
         
-        // İşlem gönderme fonksiyonu
-        async function sendTransaction() {
-            const isConnected = await isWalletConnected(); // Cüzdan bağlantısını kontrol et
-            if (!isConnected) {
-                alert("Please connect your wallet first!");
-                return;
-            }
+        // Text accueil
+        ctx.font = "bold 30px courier";
+        ctx.fillText(`Best score : ${bestScore}`, 85, 245);
+        ctx.fillText('Click to play', 90, 535);
+    }
+    // Skorları canvas'ın tepesine yazdır
+    ctx.font = "bold 20px courier";
+    ctx.fillStyle = "black";
+    ctx.fillText(`Best Score: ${bestScore}`, 10, 30);
+    ctx.fillText(`Current Score: ${currentScore}`, 200, 30);
 
-            const transaction = {
-                validUntil: Math.floor(Date.now() / 1000) + 60, // 60 sec
-                messages: [
-                    {
-                        address: "UQAfrBl6dzNH9FtFnAp-NvniYcH4sRFjuAs0_6f0bQFlu2kt", // Hedef adres
-                        amount: "100000000" // Toncoin nanotons cinsinden
-                    }
-                ]
-            };
+    document.getElementById('bestScore').innerHTML = `Best : ${bestScore}`;
+    document.getElementById('currentScore').innerHTML = `Current : ${currentScore}`;
 
-            try {
-                const result = await tonConnectUI.sendTransaction(transaction);
-                console.log(result);
-                alert("Transaction sent!");
-            } catch (error) {
-                console.error("Error sending transaction:", error);
-            }
-        }
+    // Tell the browser to perform animation
+    window.requestAnimationFrame(render);
+}
 
-        // Event listener yalnızca işlem gönderimi için
-        document.getElementById('sendTransaction').addEventListener('click', sendTransaction);
-        document.addEventListener('DOMContentLoaded', () => {
-            const user_id = window.Telegram.WebApp?.initDataUnsafe?.user?.id;
-            if (user_id) {
-                console.log("User ID:", user_id);
-            } else {
-                console.error("Telegram WebApp User ID alınamadı.");
-            }
-    
-            let id;
-            const maxAttempts = 10;
-            let attempts = 0; // attempts değişkeni başlatılıyor
+// Launch setup
+setup();
+img.onload = render;
 
-            // setInterval ile user_id'yi kontrol eden fonksiyon
-            const interval = setInterval(async () => {
-                id = window.Telegram.WebApp?.initDataUnsafe?.user?.id;
-                attempts++;
-
-                if (id) {
-                    try {
-                        const response = await fetch(`https://databasebackend-a761040b798d.herokuapp.com/api/user-status?user_id=${id}`);
-                        const data = await response.json();
-
-                        if (data.premium == 1) {
-                            document.getElementById('userStatus').innerText = 'PREMIUM';
-                        } else if (data.premium == 0){
-                            document.getElementById('userStatus').innerText = 'NORMAL';
-                        } else {
-                            document.getElementById('userStatus').innerText = 'ERROR';
-                        }
-                    } catch (error) {
-                        console.error("Error fetching user status:", error);
-                    }
-
-                    clearInterval(interval); // ID bulundu, interval durduruluyor
-                } else if (attempts >= maxAttempts) {
-                    console.error("User ID alınamadı.");
-                    clearInterval(interval); // Maksimum deneme sayısına ulaşıldı, interval durduruluyor
-                }
-            }, 500); // 500 ms aralıklarla user_id'yi kontrol eder
-        });
+// Start game
+document.addEventListener('click', () => gamePlaying = true);
+document.addEventListener('touchstart', (e) => {
+    e.preventDefault(); // Touch olayında varsayılan davranışı engelle
+    flight = jump;
+});
