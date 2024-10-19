65fd12e36e6c3d9e6803e1140a9d16ddd9019c48
BedirhanAkkurt <98023722+BedirhanAkkurt@users.noreply.github.com>
2024-10-19 20:17:03 +0300

    Add files via upload

---
+++ body.js
@@ -0,0 +1,6 @@
+document.addEventListener('DOMContentLoaded', () => {
+    window.Telegram.WebApp.ready();
+    window.Telegram.WebApp.expand();
+
+    window.Telegram.WebApp.setHeaderColor('#f0ede2');
+})
