9c8547dc9c678d062954836fd8ad3cb31fef6a64
BedirhanAkkurt <98023722+BedirhanAkkurt@users.noreply.github.com>
2024-10-19 20:27:47 +0300

    Update body.js

--- body.js
+++ body.js
@@ -1,6 +1,7 @@
 document.addEventListener('DOMContentLoaded', () => {
     window.Telegram.WebApp.ready();
     window.Telegram.WebApp.expand();
+    window.Telegram.WebApp.disableScrollExpansion();
 
-    window.Telegram.WebApp.setHeaderColor('#f0ede2');
+    window.Telegram.WebApp.setHeaderColor('#1d313c');
 })
