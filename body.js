c22adc16b98f8b9c323b5afa791d6bab23f008a1
BedirhanAkkurt <98023722+BedirhanAkkurt@users.noreply.github.com>
2024-10-25 23:40:33 +0300

    Delete body.js

--- body.js
+++
@@ -1,6 +0,0 @@
-document.addEventListener('DOMContentLoaded', () => {
-    window.Telegram.WebApp.ready();
-    window.Telegram.WebApp.expand();
-    window.Telegram.WebApp.disableVerticalSwipes();
-    window.Telegram.WebApp.setHeaderColor("#1d313c");
-})
